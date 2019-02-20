1f08d84a6849d05089ffbe428346df0aa2d19d6f
Uday <chintala.uday@gmail.com>
2019-02-21 00:56:24 +0530

    init

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+//functions
+const functions = require('firebase-functions');
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  return original;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+//   return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+//     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//     return res.redirect(303, snapshot.ref.toString());
+//   });
+});
--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
+
+  componentDidMount(){
+    var config = {
+      apiKey: "AIzaSyB4-djggJDQztUAR4ztH_olPBK1yWXOYfw",
+      authDomain: "reactandfirebase-22a10.firebaseapp.com",
+      databaseURL: "https://reactandfirebase-22a10.firebaseio.com",
+      projectId: "reactandfirebase-22a10",
+      storageBucket: "reactandfirebase-22a10.appspot.com",
+      messagingSenderId: "733534171441"
+    };
+    firebase.initializeApp(config);
+    // var database = firebase.database().ref('');
+   
+
+    // var database = firebase.database().ref('userData/SampleUserData');
+    // database.on('value', function(snapshot) {
+    //   console.log("Hello world:on:"+JSON.stringify(snapshot));
+    // });
+
+    // var database1 = firebase.database().ref('userData/SampleUserData');
+    // database1.set({ // PERMISSION_DENIED: Permission denied
+    //   Name:'sai',
+    //   Age:'25',
+    //   Address:'Karimnagar'
+    // });
+    fetch("https://us-central1-reactandfirebase-22a10.cloudfunctions.net/addMessage")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log("got fetched"+JSON.stringify(result));
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log("got error"+JSON.stringify(error));
+        }
+      )
+  }
+
   render() {
     return (
       <div className="App">
